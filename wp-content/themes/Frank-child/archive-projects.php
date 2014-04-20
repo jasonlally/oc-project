08872cd826995afaf1edc72f8cb30aed703c8afa
Jason Lally <jason.lally@gmail.com>
2014-04-19 19:10:01 -0700

    update project archive page structure

--- wp-content/themes/Frank-child/archive-projects.php
+++ wp-content/themes/Frank-child/archive-projects.php
@@ -12,13 +12,19 @@
 		<?php if ($i == 1 || ($i > 3 && $i % 2 == 0)):?>
 		<div class="row <?php echo $i == 1 ? 'first_row' : 'other_row'?>">
 		<?php endif;?>
-		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><div class="<?php echo $i < 4 ? 'one_third' : 'one_half'?> <?php if($i == 3 || ($i > 3 && $i % 2 != 0)) echo 'last_column'?> project-block">
+		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><div class="one_third col <?php if($i == 3) echo 'last_column'?> project-block">
+			<?php echo(types_render_field( "feature-image", array( "alt" => "Image representing project","proportional" => "true", "style" => "width: 100%" ) ));?>
 			<header class="project-header">
 				<h1 class="project-title"><?php the_title(); ?></h1>
 			</header>
-			<p><?php echo(types_render_field("description-one-line", array()))?></p>
+			<div class="project-description">
+				<p>
+					Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value.
+					<?php //echo(types_render_field("description-one-line", array()))?>
+				</p>
+			</div>
 		</div></a>
-		<?php if ($i == 3 || ($i > 3 && $i % 2 != 0)):?>
+		<?php if ($i > 2 & $i % 3 == 0):?>
 		</div><!--/row-->
 		<?php endif;?>
 		<?php $i++; ?>
